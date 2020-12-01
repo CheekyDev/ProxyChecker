3d68091019d52f01dda107ad45f2bd60156106cf
Cheeky <misha33759@gmail.com>
2020-12-01 15:46:17 +0500

    <m>

---
+++ ProxyChecker/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace ProxyChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(324, 22);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(215, 223);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "https://www.google.ru/\nhttps://2ip.ru/\nhttps://vk.com\nhttps://www.youtube.com\nhtt" +
+    "ps://yandex.ru/\nhttps://mail.ru/\nhttps://ok.ru/\nhttps://steamcommunity.com/";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 292);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(527, 148);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 273);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Log";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(328, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Links";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(16, 225);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 32);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Check 1";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(186, 225);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(121, 32);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Check 2";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(183, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Proxy [0]";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 452);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ProxyChecker/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Leaf.xNet;
+using System.IO;
+
+namespace ProxyChecker
+{
+    public partial class Form1 : Form
+    {
+        List<string> Proxy1 { get; set; } = new List<string>();
+
+        string path1 = "C:\\Users\\misha\\Desktop\\Proxy.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Open proxy
+        List<string> OpenProxy(string Path)
+        {
+            List<string> Proxy = File.ReadAllLines(Path).ToList();
+            return Proxy;
+        }
+
+         
+        private void button1_Click(object sender, EventArgs e)//open
+        {
+            Proxy1 = OpenProxy(path1);
+            label3.Text = $"Proxy [{Proxy1.Count}]";
+        }
+        #endregion
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string n = "\n";
+            for (int i = 0; i < richTextBox1.Lines.Count(); i++)
+            {
+                string link = richTextBox1.Lines[i];
+                richTextBox2.AppendText(link + n + Request.Get(link, Proxy1[0].Replace(" ", "")) + n);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Request
+    {
+        public static string Get(string Link, string Proxy) //Leaf.xNet.HttpException
+        {
+            HttpRequest request = new HttpRequest();
+            request.KeepAlive = true;
+            request.UserAgentRandomize();
+            try
+            {
+                request.Proxy = ProxyClient.Parse(ProxyType.Socks5, Proxy);
+                request.Get(Link).ToString();
+
+            }
+            catch
+            {
+                request.Proxy = ProxyClient.Parse(ProxyType.Socks4, Proxy);
+            }
+
+            //Httpresponse и проверять значение, вместо параши снизу
+            string response = "";
+            try
+            {
+                request.Get(Link).ToString();
+                response = "ok";
+            }
+            catch (HttpException err)
+            {
+                response = err.Message;
+            }
+            return response;
+        }
+    }
+}
